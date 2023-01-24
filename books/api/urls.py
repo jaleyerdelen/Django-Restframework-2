c79b7abe7b607c15cb79f84ab36087ba743bf5d0
jaleyerdelen <jaleyerdelen@gmail.com>
2023-01-24 17:45:17 +0300

    Concrete View

--- books/api/urls.py
+++ books/api/urls.py
@@ -3,4 +3,6 @@ from books.api import views as api_views
 
 urlpatterns = [
     path('books/', api_views.BookListCreateAPIView.as_view(), name='book-list'),
+    path('books/<int:pk>', api_views.BookDetailAPIView.as_view(), name='book-detail'),
+
 ]
--- books/api/views.py
+++ books/api/views.py
@@ -1,16 +1,28 @@
 from rest_framework.generics import GenericAPIView
 from rest_framework.mixins import ListModelMixin, CreateModelMixin
 
+from rest_framework import generics
+
 from books.api.serializers import BookSerializer, CommentSerializer
 from books.models import Book
 
-class BookListCreateAPIView(ListModelMixin, CreateModelMixin, GenericAPIView):
-    
+#Concrete View
+class BookListCreateAPIView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
+class BookDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
 
-    def post(self, request, *args, **kwargs):
-         return self.create(request, *args, **kwargs)
+#GenericAPIView
+# class BookListCreateAPIView(ListModelMixin, CreateModelMixin, GenericAPIView):
+    
+#     queryset = Book.objects.all()
+#     serializer_class = BookSerializer
+
+#     def get(self, request, *args, **kwargs):
+#         return self.list(request, *args, **kwargs)
+
+#     def post(self, request, *args, **kwargs):
+#          return self.create(request, *args, **kwargs)
