dae775d92bf788f46302117254bcc9a19e056863
jaleyerdelen <jaleyerdelen@gmail.com>
2023-01-24 17:17:09 +0300

    GenericAPIView

--- books/api/views.py
+++ books/api/views.py
@@ -1,1 +1,16 @@
+from rest_framework.generics import GenericAPIView
+from rest_framework.mixins import ListModelMixin, CreateModelMixin
+
 from books.api.serializers import BookSerializer, CommentSerializer
+from books.models import Book
+
+class BookListCreateAPIView(ListModelMixin, CreateModelMixin, GenericAPIView):
+    
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+         return self.create(request, *args, **kwargs)
